ef436b31950a5298e93dfb7cb50a8d2ccea735f3
Abu Talha <talha007abu@gmail.com>
2025-07-12 00:23:21 +0600

    day 01

---
+++ app.js
@@ -0,0 +1,16 @@
+// variable
+
+ let myName = 'Mahir Asief';
+
+ myName = 'Sr Setu'
+ console.log(myName)
+
+ const browserLanguage = 'Javascript';
+
+ var lastName = 'Rahman';
+
+ //console.log(window.lastName);
+ console.log(lastName);
+
+ //console.log(window.myName);
+// console.log(browserLanguage);
